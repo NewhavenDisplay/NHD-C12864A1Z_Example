31346d8fb4ab1965d610f14c19ad69a3cd6a5a97
NHD-DOMAIN\CJohnson <cjohnson@newhavendisplay.com>
2022-02-25 17:02:21 -0600

    Fixed issue #1.

--- NHD-C12864A1Z/NHD-C12864A1Z.ino
+++ NHD-C12864A1Z/NHD-C12864A1Z.ino
@@ -200,7 +200,7 @@ comm_write(0xC8);	  // COM direction scan
 comm_write(0xA2);   // LCD bias set
 comm_write(0x2F);   // Power Control set
 comm_write(0x26);   // Resistor Ratio Set 
-comm_write(0x81);   // Electronic Volume Command (set contrast) Double Btye: 1 of 2
+comm_write(0x81);   // Electronic Volume Command (set contrast) Double Byte: 1 of 2
 comm_write(0x11);	  // Electronic Volume value (contrast value) Double Byte: 2 of 2
 comm_write(0xAF);   // Display ON
 }
